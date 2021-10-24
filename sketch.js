b8aff8dbf9cf71a5febe7850a086ec318405e917
Gautham6 <gauthammiddiboyina@gmail.com>
2021-10-24 18:30:20 +0530

    game in database

--- sketch.js
+++ sketch.js
@@ -1,12 +1,13 @@
 var canvas, backgroundImage;
 
+
 var gameState = 0;
 var playerCount;
 
 var database;
 
 var form, player, game;
-
+var allPlayers;
 
 function setup(){
   canvas = createCanvas(400,400);
@@ -18,4 +19,10 @@ function setup(){
 
 
 function draw(){
+  if(playerCount == 4){
+    game.update(1);
+  }
+  if(gameState == 1){
+    game.play();
+  }
 }
